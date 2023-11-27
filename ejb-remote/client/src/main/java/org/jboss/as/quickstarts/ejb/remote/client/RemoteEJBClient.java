1e908117b402d6e3c5b2bc1d039afe3f0d277afa
Richard Achmatowicz <rachmato@redhat.com>
2023-11-27 15:46:41 -0500

    Working example

--- ejb-remote/client/src/main/java/org/jboss/as/quickstarts/ejb/remote/client/RemoteEJBClient.java
+++ ejb-remote/client/src/main/java/org/jboss/as/quickstarts/ejb/remote/client/RemoteEJBClient.java
@@ -116,12 +116,12 @@ public class RemoteEJBClient {
         jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
         if(Boolean.getBoolean(HTTP)) {
             //use HTTP based invocation. Each invocation will be a HTTP request
-            jndiProperties.put(Context.PROVIDER_URL,"http://localhost:8080/wildfly-services");
-//            jndiProperties.put(Context.PROVIDER_URL,"https://localhost:8443/wildfly-services");
+//            jndiProperties.put(Context.PROVIDER_URL,"http://localhost:8080/wildfly-services");
+            jndiProperties.put(Context.PROVIDER_URL,"https://localhost:8443/wildfly-services");
         } else {
             //use HTTP upgrade, an initial upgrade requests is sent to upgrade to the remoting protocol
-            jndiProperties.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
-//            jndiProperties.put(Context.PROVIDER_URL,"remote+https://localhost:8443");
+//            jndiProperties.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
+            jndiProperties.put(Context.PROVIDER_URL,"remote+https://localhost:8443");
         }
         System.out.println("lookupRemoteStateless:using JNDI Properties map: " + jndiProperties);
 
@@ -146,7 +146,7 @@ public class RemoteEJBClient {
         // the whole package name.
 
         // let's do the lookup
-        return (RemoteCalculator) context.lookup("ejb:/ejb-remote-server-side-debug/CalculatorBean!"
+        return (RemoteCalculator) context.lookup("ejb:/ejb-remote-server-side/CalculatorBean!"
             + RemoteCalculator.class.getName());
     }
 
@@ -191,7 +191,7 @@ public class RemoteEJBClient {
         // the whole package name.
 
         // let's do the lookup
-        return (RemoteCounter) context.lookup("ejb:/ejb-remote-server-side-debug/CounterBean!"
+        return (RemoteCounter) context.lookup("ejb:/ejb-remote-server-side/CounterBean!"
             + RemoteCounter.class.getName() + "?stateful");
     }
 }
